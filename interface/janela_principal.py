5a54478e6948ff24330b84ac1660cee9feb7b204
Arthurito37 <arthursantana364@outlook.com>
2026-08-08 14:02:16 -0300

    Atualizacao do projeto

---
+++ interface/janela_principal.py
@@ -0,0 +1,189 @@
+import os 
+import tkinter as tk # Importa a biblioteca que possibilitar criar interface grafica
+from tkinter import filedialog, ttk, messagebox
+import pandas as pd
+from leitura.leitor_xml import LeitorXML # Chama o metodo LeitorXML la do arquivo leitor_XML
+from tqdm import tqdm #  Barra de carregamento
+from datetime import datetime
+
+# Cria a classe Janela Principal
+class JanelaPrincipal:
+
+    # Inicializa a janela principal
+    def __init__(self):
+        self.janela = tk.Tk() #Cria a janela principal do programa
+        self.janela.title("Leitor de XML 2.0") # Define o título da janela
+        self.janela.minsize(1000, 700) # Define o tamanho mínimo da janela
+
+        # Define o Frame
+        self.frame_cabecalho = tk.Frame( # Cria o Frame do cabeçalho
+            self.janela, # O Frame pertence à janela principal
+            bg="lightblue", # Define a cor de fundo do Frame
+            height=80 # Define a altura do Frame
+        )
+        self.frame_cabecalho.pack(fill="x") # Faz o Frame ocupar toda a largura da janela
+
+        # Cria um atributo chamado Label escrito bem vindo
+        self.label_boas_vindas = tk.Label( # Cria o Frame do cabeçalho
+            self.frame_cabecalho, # O Label pertence ao Frame do cabeçalho
+            text="Bem-vindo ao Leitor de XML 2.0", # Texto exibido na tela
+            font=("Arial", 18, "bold") # Fonte Arial, tamanho 18 e em negrito
+        )
+        self.label_boas_vindas.pack(pady=20) # Exibe o Label e adiciona um espaçamento vertical
+
+        self.frame_botoes = tk.Frame(self.janela) # Cria o Frame do botao importar XML
+
+        self.frame_botoes.pack(pady=20) # Exibe o Label e adiciona um espaçamento vertical
+
+        self.botao_importar = tk.Button( # Cria o botao
+            self.frame_botoes, # Coloca o botao dentro do frame
+            text="Importar XML", # Texto exibido dentro do botao
+            font=("Arial"), # Fonte de dentro do botao
+            command=self.importar_xml, # Comando que chama a funcao importar_xml
+            width= 21, # Altura do botao
+            height= 4 # Largura do botao
+            
+        )
+        self.botao_importar.pack() # Exibe o Label e adiciona um espaçamento vertical
+
+        self.informacoes_tabela = tk.Label( # Cria um label com informacoes relacionadas as informacoes do XML
+            self.janela,
+            text="XMLs Importados = 0      Valor Total = 0"      "Periodo de Emissao:",
+            font=("Arial", 12)
+            ) # Cria o Frame das informacoes da tabela
+
+        self.informacoes_tabela.pack(pady=20) # Exibe o Label e adiciona um espaçamento vertical
+
+        self.barra_progresso = ttk.Progressbar( # Cria uma barra de progresso
+            self.janela,
+            orient="horizontal",
+            length=400,
+            mode="determinate"
+        )
+
+        self.barra_progresso.pack(pady=20) #Exibe a barra de carregamento
+
+        # Define as colunas que a tabela vai ter
+        colunas = ("Tipo", 
+                   "Numero", 
+                   "Serie", 
+                   "Data", 
+                   "Emitente", 
+                   "Valor", 
+                   "Pagamento")
+
+        # Cria o Treeview (a tabela) dentro da janela, usando as colunas definidas
+        # O parâmetro show="headings" garante que só as colunas definidas apareçam,
+        # removendo a coluna extra "#0" que o Treeview cria por padrão.
+        self.tabela = ttk.Treeview (
+            self.janela, 
+            columns=colunas, 
+            show="headings")
+
+        # Define o título (cabeçalho) da coluna "Tipo"
+        self.tabela.heading("Tipo", text="Tipo:")
+
+        # Define o título da coluna "Numero"
+        self.tabela.heading("Numero", text="Número da nota:")
+
+        # Define o título da coluna "Serie"
+        self.tabela.heading("Serie", text="Número de Serie:")
+
+        # Define o título da coluna "Data"
+        self.tabela.heading("Data", text="Data:")
+
+        # Define o título da coluna "Emitente"
+        self.tabela.heading("Emitente", text="Emitente:")
+
+        # Define o título da coluna "Valor"
+        self.tabela.heading("Valor", text="Valor:")
+
+        # Define o título da coluna "Pagamento"
+        self.tabela.heading("Pagamento", text="Forma de Pagamento:")
+
+    
+        
+        # Exibe a tabela na janela
+        self.tabela.pack()
+    
+
+    def importar_xml(self): # Cria a funcao importar_xml
+        caminho = filedialog.askdirectory() # Abre o explorador de pastas
+
+        leitor = LeitorXML() # Cria um objeto da classe LeitorXML, responsável por abrir e ler os arquivos XML.
+
+        if caminho: # Se a variavel caminho estiver preenchida escreve no console a pasta que abriu
+            print(caminho) 
+        else: # Se nao escreve no console que nenhuma pasta foi selecionada
+            print("Nenhuma pasta foi selecionada.")
+
+        quantidade_xml = 0 # Quantidade de XML
+
+        arquivos = os.listdir(caminho) # Lista todos os arquivos da pasta selecionada
+        
+        quantidade_xml = 0 # Declara a quantidade de XML importados como zero
+
+        valor_total = 0 # Valor total de dinheiro dos XMLs lidos
+
+        total_arquivos = len(arquivos)
+
+        menor_data = None # Variavel que armazena a menor data de emissao do xml
+
+        maior_data = None # Variavel que armazena a maior data de emissao do xml
+
+        for arquivo in arquivos: # Percorre cada arquivo da lista
+
+            if arquivo.endswith(".xml"): # Se for encontrado um arquivo que for .xml ele escrevera o XML no console
+                
+                print(arquivo)
+                caminho_arquivo = os.path.join(caminho, arquivo) # Junta o caminho da pasta com o nome do arquivo para obter o caminho completo do XML.
+
+                dados = leitor.abrir_xml(caminho_arquivo) # Chama o método abrir_xml() para abrir e ler o arquivo XML.
+
+                data_atual = datetime.strptime(dados[3], "%d/%m/%Y %H:%M:%S")
+
+                if menor_data is None:
+                    menor_data = data_atual
+                    maior_data = data_atual
+                else:
+                    if data_atual < menor_data:
+                        menor_data = data_atual
+
+                    if data_atual > maior_data:
+                        maior_data=data_atual    
+
+                valor_total += dados[5]
+
+                self.tabela.insert("", "end", values=dados)
+
+                quantidade_xml += 1 # acrescenta um no contador de XML
+                progresso = (quantidade_xml / total_arquivos) * 100
+
+
+
+            self.barra_progresso["value"] = progresso
+            self.janela.update_idletasks()
+
+
+
+
+
+                
+
+
+        print("Menor data:", menor_data)
+        print("Maior data:", maior_data)
+        print(f"Foram encontrados {quantidade_xml} arquivos XML.") # Escreve no console a quantidade de XML lido.
+        print(f"O valor total R$: {valor_total}") # Escreve no console o total
+
+        # Atualiza as informacoes de quantidade xml lido.
+        self.informacoes_tabela.config(
+            text=f"XMLs importados: {quantidade_xml}      Valor Total R$: {valor_total:.2f}      Periodo de Emissao: {menor_data} até {maior_data}",
+            )
+
+
+
+
+    # Inicia a interface gráfica
+    def executar(self):
+        self.janela.mainloop()
---
+++ leitura/leitor_xml.py
@@ -0,0 +1,431 @@
+import xml.etree.ElementTree as ET # Importa a biblioteca responsável por ler e interpretar arquivos XML.
+from datetime import datetime
+
+# Classe responsável por abrir e ler os arquivos XML.
+class LeitorXML:
+
+    # Inicializa a classe LeitorXML.
+    def __init__(self):
+        pass # Ainda não há nenhuma inicialização necessária.
+
+    def formatar_cnpj(self, doc):
+        # Se tiver 11 dígitos → CPF
+        if len(doc) == 11:
+            return f"{doc[:3]}.{doc[3:6]}.{doc[6:9]}-{doc[9:]}"
+    
+        #Se tiver 14 dígitos → CNPJ
+        elif len(doc) == 14:
+            return f"{doc[:2]}.{doc[2:5]}.{doc[5:8]}/{doc[8:12]}-{doc[12:]}"
+        else:
+            return "Documento inválido"
+
+    def formatar_data(self, data_iso):
+        # Converte a string ISO para objeto datetime
+        dt = datetime.fromisoformat(data_iso)
+        # Formata no padrão brasileiro
+        return dt.strftime("%d/%m/%Y %H:%M:%S")
+
+    def formatar_dinheiro(self, cash):
+        # cash já é string, então só troca o ponto pela vírgula
+        return cash.replace(".", ",")
+
+
+        
+    # Abre um arquivo XML a partir do caminho informado.
+    def abrir_xml(self, caminho_xml): 
+        arvore = ET.parse(caminho_xml) # Lê o arquivo XML e cria uma árvore com todos os seus elementos.
+
+        # Obtém a raiz da árvore (primeira tag do XML).
+        raiz = arvore.getroot()
+        # Define o namespace utilizado no XML da NF-e/NFC-e.
+        namespace = {
+            "nfe": "http://www.portalfiscal.inf.br/nfe"
+        }
+
+        # Procura a tag NFe dentro da raiz do XML.
+        NFe = raiz.find("nfe:NFe", namespace)
+
+        # Procura a tag infNFe dentro da tag NFe.
+        infNFe = NFe.find("nfe:infNFe", namespace)
+
+        # Procura a tag ide, que contém as informações de identificação da nota fiscal.
+        ide = infNFe.find("nfe:ide", namespace)
+
+
+
+        # Extrai o modelo do documento fiscal (55 = NF-e, 65 = NFC-e).
+        modeloNota = ide.find("nfe:mod", namespace)
+
+        # Exibe no terminal o modelo da nota fiscal.
+        if modeloNota.text == "65":
+            print("Tipo: NFCe")
+            modeloNota = "NFCe"
+        else:
+            print("Tipo: NFe")
+            modeloNota = "NFe"
+
+
+        # Extrai a série da nota fiscal.
+        numeroSerie = ide.find("nfe:serie", namespace)
+
+        # Exibe no terminal a série da nota fiscal.
+        print(f"Numero de serie da nota {numeroSerie.text}.")
+
+        # Extrai o número da nota fiscal.
+        numeroNota = ide.find("nfe:nNF", namespace)
+
+        # Exibe no terminal o número da nota fiscal.
+        print(f"O numero da nota e {numeroNota.text}.")
+
+        # Extrai a data e hora de emissão da nota fiscal.
+        dataEmissao = ide.find("nfe:dhEmi", namespace)
+
+        # Exibe no terminal a data e hora de emissão.
+        print(f"Data da emissao da nota {dataEmissao.text}.")
+        dataEmissao = self.formatar_data(dataEmissao.text)
+
+        # Procura a data e hora de saída/entrada da mercadoria (essa tag pode não existir).
+        dataSaida = ide.find("nfe:dhSaiEnt", namespace)
+
+        # Verifica se a tag dhSaiEnt foi encontrada antes de acessar seu conteúdo.
+        if dataSaida is not None:
+            print(f"Data de saida da nota {dataSaida.text}.")
+            dataSaida = self.formatar_data(dataSaida.text)
+
+# --------------------------------------- Emitente -----------------------------------------
+
+        # Procura a tag emit que contem as informacoes do emitente
+        emit = infNFe.find("nfe:emit", namespace)
+
+        # Procura a tag emit que contem o numero do CNPJ
+        numeroCNPJ = emit.find("nfe:CNPJ", namespace)
+
+        # Exibe no terminal o CNPJ
+        print(f"CNPJ: {numeroCNPJ.text}.")
+        numeroCNPJ = self.formatar_cnpj(numeroCNPJ.text)
+
+        # Procura a tag emit que contem a razao social
+        razaoSocial = emit.find("nfe:xNome", namespace)
+
+        # Exibe no terminal a razao social do emitente.
+        print(f"Razão Social: {razaoSocial.text}.")
+
+        # Extrai o nome fantasia do emitente.
+        nomeFantasia = emit.find("nfe:xFant", namespace)
+
+        # Se tiver nome fantasia
+        if nomeFantasia is not None:
+            print(f"Nome Fantasia: {nomeFantasia.text}.")
+
+        # Extrai a inscrição estadual do emitente.
+        inscricaoEstadual = emit.find("nfe:IE", namespace)
+
+        # Exibe no terminal a IE do emitente
+        print(f"Inscrição Estadual: {inscricaoEstadual.text}.")
+
+        # Procura a tag enderEmit onde tem o endereco do emitente
+        enderEmit = emit.find ("nfe:enderEmit", namespace)
+
+        # Extrai o logradouro do emitente.
+        logradouro = enderEmit.find("nfe:xLgr", namespace)
+
+        # Exibe no terminal o logradouro
+        print(f"Logradouro: {logradouro.text}.")
+
+        # Extrai o numero do endereco do emitente.
+        numeroEndereco = enderEmit.find("nfe:nro", namespace)
+
+        # Exibe no terminal o numero do endereco
+        print(f"Numero: {numeroEndereco.text}.")
+
+        # Extrai o bairro do emitente.
+        bairro = enderEmit.find("nfe:xBairro", namespace)
+
+        # Exibe no terminal o bairro
+        print(f"Bairro {bairro.text}.")
+
+        # Extrai o municipio do emitente.
+        municipio = enderEmit.find("nfe:xMun", namespace)
+
+        # Exibe no terminal o municipio
+        print(f"Municipio {municipio.text}.")
+
+        # Extrai o uf do emitente.
+        uf = enderEmit.find("nfe:UF", namespace)
+
+        # Exibe no terminal o UF
+        print(f"UF: {uf.text}.")
+
+        # Extrai o cep do emitente.
+        cep = enderEmit.find("nfe:CEP", namespace)
+
+        # Exibe no terminal o CEP
+        print(f"CEP: {cep.text}.")
+
+# --------------------------------------- Destinatario -----------------------------------------
+        # Procura o destinatario
+        dest = infNFe.find("nfe:dest", namespace)
+
+        # Se o destinatario estiver
+        if dest is not None:
+
+            #Procura o campo CPF ou CNPJ
+            numeroCPF = dest.find("nfe:CPF", namespace)
+
+            # Se tiver o CPF:
+            if numeroCPF is not None:
+
+                #Escreve o CPF
+                print(f"CPF: {numeroCPF.text}")
+                numeroCPF = self.formatar_cnpj(numeroCPF.text)
+
+            # Procura o campo nome:
+            nomeDestinatario = dest.find("nfe:xNome", namespace)
+
+            # Se tiver o nome
+            if nomeDestinatario is not None:
+
+                # Escreve o nome:
+                print(f"Nome: {nomeDestinatario.text}")
+
+# --------------------------------------- Impostos -----------------------------------------
+
+        # Procura o total
+        total = infNFe.find("nfe:total", namespace)
+
+        # Procura no ICMStot
+        ICMSTot = total.find("nfe:ICMSTot", namespace)
+
+        # Procura o valor do produtos
+        valorProdutos = ICMSTot.find("nfe:vProd", namespace)
+
+        # Escreve o valor dos produtos
+        print(f"Valor dos Produtos: {valorProdutos.text}")
+        valorProdutos = self.formatar_dinheiro(valorProdutos.text)
+
+        # Procura o valor da nota
+        valorNota = ICMSTot.find("nfe:vNF", namespace)
+
+        # Escreve o valor da nota
+        print(f"Valor da Nota: {valorNota.text}")
+        valorNota = float(valorNota.text)
+
+        # Procura o valor do desconto
+        valorDesconto = ICMSTot.find("nfe:vDesc", namespace)
+        
+
+        # Escreve o valor do desconto
+        print(f"Valor do Desconto: {valorDesconto.text}")
+        valorDesconto = self.formatar_dinheiro(valorDesconto.text)
+
+        # Procura o valor do frete
+        valorFrete = ICMSTot.find("nfe:vFrete", namespace)
+        
+
+        # Escreve o valor do frete
+        print(f"Valor do Frete: {valorFrete.text}")
+        valorFrete = self.formatar_dinheiro(valorFrete.text)
+
+        # Procura o ICMS
+        valorICMS = ICMSTot.find("nfe:vICMS", namespace)
+        
+
+        # Escreve o nome:
+        print(f"Valor do ICMS: {valorICMS.text}")
+        valorICMS = self.formatar_dinheiro(valorICMS.text)
+
+        # Procura o valor do IP
+        valorIPI = ICMSTot.find("nfe:vIPI", namespace)
+
+        # Se o IP estiver
+        if valorIPI is not None:
+            print(f"Valor do IPI: {valorIPI.text}")
+
+# --------------------------------------- Pagamento -----------------------------------------
+        # Procura a tag pag
+        pag = infNFe.find("nfe:pag", namespace)
+
+        # Procura a tag detPag
+        detPag = pag.find("nfe:detPag", namespace)
+
+        # Procura o numero da forma de pagamento
+        formaPagamento = detPag.find("nfe:tPag", namespace)
+
+        # Faz o tratamento dos valores 
+        if formaPagamento.text == "01":
+
+            # Escreve a forma de pagamento
+            print("Forma de pagamento: Dinheiro")
+            formaPagamento = "DINHEIRO"
+
+        elif formaPagamento.text == "03":
+            # Escreve a forma de pagamento
+            print("Forma de pagamento: Cartao de Credito")
+            formaPagamento = "CARTAO DE CREDITO"
+
+        elif formaPagamento.text == "04":
+            # Escreve a forma de pagamento
+            print("Forma de pagamento: Cartao de Debito")
+            formaPagamento = "CARTAO DE DEBITO"
+        elif formaPagamento.text == "17":
+
+            # Escreve a forma de pagamento
+            print("Forma de pagamento: PIX")
+            formaPagamento = "PIX"
+
+        elif formaPagamento.text == "99":
+            # Escreve a forma de pagamento
+            print("Forma de pagamento: Outros")
+            formaPagamento = "OUTROS"
+
+        else:
+            print("Forma de pagamento nao encontrada")
+
+        # Procura o valor pago
+        valorPago = detPag.find("nfe:vPag", namespace)
+
+        #Escreve o valor pago
+        print(f"O valor pago: {valorPago.text}")
+        valorPago = self.formatar_dinheiro(valorPago.text)
+
+        # Procura o valor do troco.
+        valorTroco = pag.find("nfe:vTroco", namespace)
+
+        # Verifica se existe troco.
+        if valorTroco is not None:
+            print(f"Troco: {valorTroco.text}")
+
+# --------------------------------------- Produtos -----------------------------------------
+        # Procura a lista de produtos na tag det
+        produtos = infNFe.findall("nfe:det", namespace)
+
+        # Para cada produto encontrado
+        for produto in produtos: 
+
+            # Procura os produtos na tag prod
+            prod = produto.find("nfe:prod", namespace)
+
+            # Procura o codigo do produto
+            codigo = prod.find("nfe:cProd", namespace)
+
+            # Escreve o codigo do produto
+            print(f"Código: {codigo.text}")
+
+            # Procura a descricao do produto
+            descricao = prod.find("nfe:xProd", namespace)
+
+            # Escreve a descricao do produto
+            print(f"Descrição: {descricao.text}")
+
+            # Procura o NCM do produto
+            ncm = prod.find("nfe:NCM", namespace)
+
+            # Escreve o NCM do produto
+            print(f"NCM: {ncm.text}")
+
+            # Procura o CFOP do produto
+            cfop = prod.find("nfe:CFOP", namespace)
+
+            # Escreve o CFOP do produto
+            print(f"CFOP: {cfop.text}")
+
+            # Procura a unidade 
+            unidade = prod.find("nfe:uCom", namespace)
+
+            #Escreve a unidade
+            print(f"Unidade: {unidade.text}")
+
+            # Procura a quantidade do produto
+            quantidade = prod.find("nfe:qCom", namespace)
+
+            # Escreve a quantidade do produto
+            print(f"Quantidade: {quantidade.text}")
+
+            # Procura o valor do produto
+            valorUnitario = prod.find("nfe:vUnCom", namespace)
+
+            # Escreve o valor do produto 
+            print(f"Valor Unitário: {valorUnitario.text}")
+
+            # Procura o valor do total quantidade vezes valor do produto
+            valorProduto = prod.find("nfe:vProd", namespace)
+
+            # Escreve o valor total
+            print(f"Valor Total: {valorProduto.text}")
+
+            # Escreve quarenta -
+            print("-" * 40)
+
+# --------------------------------------- Identificacao -----------------------------------------
+
+        # Procura a tag infNFe dentro da tag NFe.
+        chaveNota = infNFe.get("Id")
+        print(f"Chave da nota:{chaveNota}")
+
+        # Procura a tag NFe dentro da raiz do XML.
+        protNFe = raiz.find("nfe:protNFe", namespace)
+
+        # Procura a tag protocolos
+        infProt = protNFe.find("nfe:infProt", namespace)
+
+        # Procura o Protocolo de autorizacao da nota
+        protocoloNota = infProt.find("nfe:nProt", namespace)
+
+        # Escreve o protocolocad anota
+        print(f"Procolo da nota: {protocoloNota.text}")
+
+        # Procura a autorizacao da nota
+        dataAutorizacao = infProt.find("nfe:dhRecbto", namespace)
+
+        # Escreve a nota
+        print(f"Horario de Autorizacao:{dataAutorizacao.text}")
+        dataAutorizacao = self.formatar_data(dataAutorizacao.text)
+
+        return (
+            # Indica se a nota corresponde a NFCe ou NFe
+            modeloNota,
+            # Numero da Nota
+            numeroNota.text,
+            #Numero de Serie
+            numeroSerie.text,
+            # Data de Emissao do XML
+            dataEmissao,
+            # Numero do XML
+            numeroCNPJ,
+            # Valor total da nota
+            valorNota,
+            # Forma de pagamento
+            formaPagamento
+        )
+
+
+            
+
+
+
+
+
+
+
+
+        
+
+
+        
+        
+
+
+
+        
+
+ 
+
+
+
+
+
+
+
+        
+        
+        
